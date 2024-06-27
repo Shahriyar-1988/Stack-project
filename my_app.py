e047beb9b59c774882a27fe00ccdc3cbafe8a52a
Shahriyar-1988 <116467943+Shahriyar-1988@users.noreply.github.com>
2024-06-26 19:24:17 -0700

    Add files via upload

--- my_app.py
+++ my_app.py
@@ -7,34 +7,49 @@ model = joblib.load("xgbmodel.joblib")
 st.title('Salary Prediction in 2023')
 st.write("### We need some information to predict the salary")
 
-countries = [
-    'Other' 'United Kingdom of Great Britain and Northern Ireland'
- 'United States of America' 'Netherlands' 'Germany' 'France' 'Spain'
- 'South Africa' 'Italy' 'Canada' 'Switzerland' 'Brazil' 'Australia'
- 'Norway' 'Turkey' 'Sweden' 'India' 'Poland' 'Finland' 'Portugal'
- 'Austria' 'Belgium' 'Romania' 'Denmark' 'Russian Federation' 'Israel'
- 'Ukraine' 'Czech Republic' 'Mexico' 'New Zealand'
+countries = ['Other', 'United Kingdom of Great Britain and Northern Ireland',
+       'United States of America', 'Netherlands', 'Germany', 'France',
+       'Spain', 'South Africa', 'Italy', 'Canada', 'Switzerland',
+       'Brazil', 'Australia', 'Norway', 'Turkey', 'Sweden', 'India',
+       'Poland', 'Finland', 'Portugal', 'Austria', 'Belgium', 'Romania',
+       'Denmark', 'Russian Federation', 'Israel', 'Ukraine',
+       'Czech Republic', 'Mexico', 'New Zealand'
 ]
 
 Dev_type = [
-    'Developer, full-stack' 'Developer, back-end' 'Developer, QA or test'
- 'Developer, front-end' 'other' 'Developer, mobile'
- 'Developer, embedded applications or devices'
- 'Developer, desktop or enterprise applications' 'Developer Experience'
- 'Developer, game or graphics' 'Developer Advocate'
-]
+    'Developer, full-stack',
+ 'Developer, back-end',
+ 'Developer, QA or test',
+ 'Developer, front-end',
+ 'other',
+ 'Developer, mobile',
+ 'Developer, embedded applications or devices',
+ 'Developer, desktop or enterprise applications',
+ 'Developer Experience',
+ 'Developer, game or graphics',
+ 'Developer Advocate']
 
 Education = [
-    "Bachelor's degree" 'Non-academic degree' "Master's degree"
- 'Associate degree' 'Professional degree'
+    "Bachelor's degree",
+ 'Non-academic degree',
+ "Master's degree",
+ 'Associate degree',
+ 'Professional degree'
 ]
 
 Industry = [
-    'Other'
- 'Information Services, IT, Software Development, or other Technology'
- 'Financial Services' 'Manufacturing, Transportation, or Supply Chain'
- 'Retail and Consumer Services' 'Higher Education' 'Legal Services'
- 'Insurance' 'Healthcare' 'Oil & Gas' 'Wholesale' 'Advertising Services'
+    'Other',
+ 'Information Services, IT, Software Development, or other Technology',
+ 'Financial Services',
+ 'Manufacturing, Transportation, or Supply Chain',
+ 'Retail and Consumer Services',
+ 'Higher Education',
+ 'Legal Services',
+ 'Insurance',
+ 'Healthcare',
+ 'Oil & Gas',
+ 'Wholesale',
+ 'Advertising Services'
 ]
 
 # Architecture of the Webpage
@@ -54,12 +69,9 @@ columns = ['YearsCodePro', 'DevType', 'Country', 'EdLevel', 'Industry']
     
 #    st.subheader(f"The estimated salary is ${salary[0]:.2f}")
 if ok:
-    X_new = np.array([float(years_code), Dev_tp, country, Ed_tp, Ind_tp])
+    X_new = np.array([years_code, Dev_tp, country, Ed_tp, Ind_tp],dtype=object)
+    x_new[0]=np.float32(years_code)
     X_new_df = pd.DataFrame([X_new], columns=columns)
-    
-    # Debugging statements
-    st.write("Input DataFrame:")
-    st.write(X_new_df)
     
     try:
         salary = model.predict(X_new_df)
